f03512ef15d34c1c2d30bd82c100d07ff12eca83
Willy Hardy <nuptials.secrecy554@4wrd.cc>
2026-08-09 01:49:59 -0400

    Update Homebrew formula for 0.0.3

--- Formula/pixel-pusher.rb
+++ Formula/pixel-pusher.rb
@@ -1,8 +1,8 @@
 class PixelPusher < Formula
   desc "Recover clean pixel grids and compact palettes from imperfect pixel art"
   homepage "https://github.com/wjhrdy/pixel-pusher"
-  url "https://github.com/wjhrdy/pixel-pusher/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "3b7660fef906930be83b5ad2690a58ce72210eea194fcb1c3031a19fc8a6abc9"
+  url "https://github.com/wjhrdy/pixel-pusher/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "7f0a72ad9e0cf95cdc3fae34a4f0a0e5960e698dc300ed09b54ff38fbb5b0346"
 
   depends_on "rust" => :build
 
